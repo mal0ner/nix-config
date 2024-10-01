6bb292acd1d37efddd459f72f9c8ecf920a8b89a
Cameron Malone <cameronmkm2@gmail.com>
2024-10-01 15:19:30 +1000

    add kitty configuration

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     kitty
     obsidian
   ];
+
+  # move kitty configuration to correct location
+  home.file = {
+    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/config/kitty";
+  };
 }
